f64a2fb3d0951f780ed3fec39b53090ff7e48c2f
Duncan McIsaac <dbm@andrew.cmu.edu>
2014-05-06 22:00:11 -0400

    Added some permissions to instructor and user fields

--- config/routes.rb
+++ config/routes.rb
@@ -13,19 +13,19 @@ ChessCamp::Application.routes.draw do
   resources :users
   resources :sessions
 
+  # session routes
+  get 'user/edit' => 'users#edit', as: :edit_current_user
+  get 'signup' => 'users#new', as: :signup
+  get 'login' => 'sessions#new', as: :login
+  get 'logout' => 'sessions#destroy', as: :logout
+
   # semi-static routes
   get 'home', to: 'home#index', as: :home
   get 'home/about', to: 'home#about', as: :about
   get 'home/contact', to: 'home#contact', as: :contact
   get 'home/privacy', to: 'home#privacy', as: :privacy
 
-  # session routes
-  get 'instructor/edit' => 'user#edit', as: :edit_current_user # what about the instructor id?
-  get 'signup' => 'instructors#new', as: :signup
-  get 'login' => 'sessions#new', as: :login
-  get 'logout' => 'sessions#destroy', as: :logout
-
-
+  
   # set the root url
   root to: 'home#index'
 
